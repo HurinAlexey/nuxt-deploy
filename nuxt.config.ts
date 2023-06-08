e64c1a5dba0b3c1643de8b7c568e98360e33018f
Alex <hurinalex@gmail.com>
2023-06-08 17:07:57 +0300

    rebuild

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
-  app: {
-    baseURL: '/nuxt-deploy/',
+  // app: {
+  //   baseURL: '/nuxt-deploy/',
+  // },
+  devServer: {
+    port: 9000,
   },
   css: ['@/assets/styles/main.scss'],
 })
